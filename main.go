fbecce8789f804cda5e6b16041d4a5a77748af8e
Moshe Shitrit <moshe@s5t.dev>
2021-08-22 23:41:20 -0400

    Add getVersion function

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func getVersion() string {
+	return "0.0.1"
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/version", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"version": "0.0.1",
+			"version": getVersion(),
 		})
 	})
 	r.Run()
